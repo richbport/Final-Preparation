c700cb07edafd3c77979346cc34206944749a066
Rich <richbport@yahoo.com>
2025-08-04 14:02:31 -0400

    const users

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
 async function main() {
-    console.log(await (await fetch("https://jsonplaceholder.typicode.com/users")).json());
+    const users = await fetch("https://jsonplaceholder.typicode.com/users")
+    console.log(await ().json());
 }
 
 main();
