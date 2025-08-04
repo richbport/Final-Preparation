72936c0ab67fd15587dcf75df1e0181b5aa53998
Rich <richbport@yahoo.com>
2025-08-04 13:32:37 -0400

    async await

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+
+console.log(await fetch("https://jsonplaceholder.typicode.com/users").json())
