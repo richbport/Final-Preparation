8b6e2aa529423dbc657f6f5dce039a405ff45180
Rich <richbport@yahoo.com>
2025-08-04 13:56:08 -0400

    fetch users

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
+async function main() {
+    console.log(await (await fetch("https://jsonplaceholder.typicode.com/users")).json());
+}
 
-console.log(await fetch("https://jsonplaceholder.typicode.com/users").json())
+main();
