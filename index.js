1367f4aa7682614c4bba344cc19ff2a62a4faaeb
Rich <richbport@yahoo.com>
2025-08-05 19:55:53 -0400

    join usersData.map

--- index.js
+++ index.js
@@ -4,15 +4,19 @@
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
-  console.log(usersData);
-  const x = `<div class="user-card">
+  console.log(
+    usersData.map(
+      (user) => `<div class="user-card">
     <div class="user-card__container">
       <h3>User's Name</h3>
       <p><b>Email:</b> email@email.com</p>
       <p><b>Phone:</b> 0000000000</p>
       <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
     </div>
-  </div>`;
+  </div>`
+    )
+    .join("")
+  );
 }
 
 main();
