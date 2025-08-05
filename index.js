1290df3cf224ce9f71c9a44655105ce385faa203
Rich <richbport@yahoo.com>
2025-08-05 17:30:49 -0400

    user-card

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
 async function main() {
-    const users = await fetch("https://jsonplaceholder.typicode.com/users")
-    const usersData = await users.json();
-    console.log(usersData);
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const usersData = await users.json();
+  <div class="user-card">
+    <div class="user-card__container">
+      <h3>User's Name</h3>
+      <p><b>Email:</b> email@email.com</p>
+      <p><b>Phone:</b> 0000000000</p>
+      <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+    </div>
+  </div>;
 }
 
 main();
-
