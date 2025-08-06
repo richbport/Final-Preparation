2ac3bdf55f8fa873baf71f4a409f87d40752e059
Rich <richbport@yahoo.com>
2025-08-05 20:31:16 -0400

    usersData review

--- index.js
+++ index.js
@@ -4,20 +4,21 @@
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
-  const userListEl = document.querySelector('.user-list');
-  
-    userListEl.innerHTML = usersData
+  const userListEl = document.querySelector(".user-list");
+  console.log(usersData)
+  userListEl.innerHTML = usersData
     .map(
-      (user) => `<div class="user-card">
-    <div class="user-card__container">
-      <h3>User's Name</h3>
-      <p><b>Email:</b> email@email.com</p>
-      <p><b>Phone:</b> 0000000000</p>
-      <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
-    </div>
-  </div>`
+      (user) =>
+        `<div class="user-card">
+          <div class="user-card__container">
+            <h3>User's Name</h3>
+            <p><b>Email:</b> email@email.com</p>
+            <p><b>Phone:</b> 0000000000</p>
+            <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+          </div>
+        </div>`
     )
-    .join("")
+    .join("");
 }
 
 main();
