ec9cf34c194772ee3e1225f20c1a60fd7002b890
Rich <richbport@yahoo.com>
2025-08-06 14:35:15 -0400

    cleaned up order

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector(".user-list");
 
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
-  const userListEl = document.querySelector(".user-list");
-  console.log(usersData)
   userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
 }
 
 main();
 
 function userHTML(user) {
-  `<div class="user-card">
+  return `<div class="user-card">
           <div class="user-card__container">
             <h3>${user.name}</h3>
             <p><b>Email:</b> ${user.email}</p>
             <p><b>Phone:</b> ${user.phone}</p>
             <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
           </div>
-        </div>`
+        </div>`;
 }
