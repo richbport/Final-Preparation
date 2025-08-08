b7e220442c103cab94c4e7bb4401a90c63b4b6cf
Rich <richbport@yahoo.com>
2025-08-08 10:31:46 -0400

    postListEl

--- user.js
+++ user.js
@@ -1,9 +1,25 @@
-async function main() {
-    const id = localStorage.getItem("id")
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = await posts.json();
+const postListEl = document.querySelector('.post-list');
 
-    console.log(postsData)
+async function main() {
+  const id = localStorage.getItem("id");
+  const posts = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+  );
+  const postsData = await posts.json();
+
+  console.log(postsData);
+
+  postsData.map(
+    (post) =>
+      `<div class="post">
+        <div class="post__title">
+          Post Title
+        </div>
+        <p class="post__body">
+          Post Body
+        </p>
+    </div>`
+  );
 }
 
 main();
