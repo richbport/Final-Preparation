cffabbfc72ed759c5dc81e3f39100ef74209dda2
Rich <richbport@yahoo.com>
2025-08-08 09:26:54 -0400

    await posts.json()

--- user.js
+++ user.js
@@ -1,2 +1,9 @@
-console.log(localStorage.getItem("id100"));
+async function main() {
+    const id = localStorage.getItem("id")
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postsData = await posts.json();
 
+    console.log(postsData)
+}
+
+main();
