ecb2c8d30b76d5045c598d77aecca2594532a665
Rich <richbport@yahoo.com>
2025-08-12 10:43:22 -0400

    altered function renderPosts

--- user.js
+++ user.js
@@ -3,28 +3,10 @@ const id = localStorage.getItem("id");
 
 async function onSearchChange(event) {
   const id = event.target.value;
-  const posts = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
-  );
-  const postsData = await posts.json();
-  console.log(postsData);
-
-  postListEl.innerHTML = postsData
-    .map(
-      (post) =>
-        `<div class="post">
-        <div class="post__title">
-          ${post.title}
-        </div>
-        <p class="post__body">
-          ${post.body}
-        </p>
-    </div>`
-    )
-    .join("");
+  renderPosts(id);
 }
 
-async function renderPosts() {
+async function renderPosts(id) {
   const posts = await fetch(
     `https://jsonplaceholder.typicode.com/posts?userId=${id}`
   );
